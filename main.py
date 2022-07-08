fb079a10d087bf80ed7d279280500d6ebc1d4d0f
Samuel Young <sammy.young@softwire.com>
2022-07-08 15:04:04 +0100

    can now lookup a character

--- main.py
+++ main.py
@@ -8,20 +8,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-public_api_key = getenv("MARVEL_PUBLIC_API_KEY")
-private_api_key = getenv("MARVEL_PRIVATE_API_KEY")
+PUBLIC_API_KEY = getenv("MARVEL_PUBLIC_API_KEY")
+PRIVATE_API_KEY = getenv("MARVEL_PRIVATE_API_KEY")
 
-URL = "https://gateway.marvel.com:443/v1/public/characters/1"
-
-# name = input("What Marvel character? ")
+SEARCH_URL = "https://gateway.marvel.com:443/v1/public/characters"
+DETAILS_URL = "https://gateway.marvel.com:443/v1/public/characters/{id}"
 
 timestamp = str(time_ns())
-key_hash = md5(f"{timestamp}{private_api_key}{public_api_key}".encode("utf-8")).hexdigest()
-params = {
-    'apikey': public_api_key,
+key_hash = md5(f"{timestamp}{PRIVATE_API_KEY}{PUBLIC_API_KEY}".encode("utf-8")).hexdigest()
+default_params = {
+    'apikey': PUBLIC_API_KEY,
     'ts': timestamp,
     'hash': key_hash
 }
 
-res = get(URL, params=params).json()
-print(res)
+# name = input("What Marvel character? ")
+name = "Spider-Man"
+
+print("trying to find that character . . .")
+
+search_params = {
+    **default_params,
+    'nameStartsWith': name
+}
+
+res = get(SEARCH_URL, search_params).json()
+
+print(res['data']['results'][0]['name'])
