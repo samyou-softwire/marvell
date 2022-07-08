9511e855440ab9985d0d1bed363b133ec875a380
Samuel Young <sammy.young@softwire.com>
2022-07-08 15:18:30 +0100

    program now loops

--- main.py
+++ main.py
@@ -14,48 +14,49 @@ PRIVATE_API_KEY = getenv("MARVEL_PRIVATE_API_KEY")
 SEARCH_URL = "https://gateway.marvel.com:443/v1/public/characters"
 DETAILS_URL = "https://gateway.marvel.com:443/v1/public/characters/{id}"
 
-timestamp = str(time_ns())
-key_hash = md5(f"{timestamp}{PRIVATE_API_KEY}{PUBLIC_API_KEY}".encode("utf-8")).hexdigest()
-default_params = {
-    'apikey': PUBLIC_API_KEY,
-    'ts': timestamp,
-    'hash': key_hash
-}
+while True:
+    timestamp = str(time_ns())
+    key_hash = md5(f"{timestamp}{PRIVATE_API_KEY}{PUBLIC_API_KEY}".encode("utf-8")).hexdigest()
+    default_params = {
+        'apikey': PUBLIC_API_KEY,
+        'ts': timestamp,
+        'hash': key_hash
+    }
 
-name = input("What Marvel character? ")
+    name = input("What Marvel character? ")
 
-print("trying to find that character . . .")
+    print("trying to find that character . . .")
 
-search_params = {
-    **default_params,
-    'nameStartsWith': name
-}
+    search_params = {
+        **default_params,
+        'nameStartsWith': name
+    }
 
-res = get(SEARCH_URL, search_params).json()
+    res = get(SEARCH_URL, search_params).json()
 
-# refer to https://developer.marvel.com/docs#!/public/getCreatorCollection_get_0 for data shape
-characters = res['data']['results']
+    # refer to https://developer.marvel.com/docs#!/public/getCreatorCollection_get_0 for data shape
+    characters = res['data']['results']
 
-if len(characters) == 0:
-    print("Couldn't find any characters by this name...")
-else:
-    i = 0
-    IDs = []
-    print(f"Please select a character)")
-    for character in characters:
-        i += 1
-        print(f"- {i}:{character['name']}")
-        IDs.append(character['id'])
+    if len(characters) == 0:
+        print("Couldn't find any characters by this name...")
+    else:
+        i = 0
+        IDs = []
+        print(f"Please select a character)")
+        for character in characters:
+            i += 1
+            print(f"- {i}:{character['name']}")
+            IDs.append(character['id'])
 
-    index = int(input(f"Which ID? (1-{len(characters)}) "))
-    id = IDs[index]
+        index = int(input(f"Which ID? (1-{len(characters)}) ")) - 1
+        id = IDs[index]
 
-    print("Retrieving details . . .")
+        print("Retrieving details . . .")
 
-    res = get(DETAILS_URL.format(id=id), params=default_params).json()
+        res = get(DETAILS_URL.format(id=id), params=default_params).json()
 
-    character = res['data']['results'][0]  # should only be one returned
+        character = res['data']['results'][0]  # should only be one returned
 
-    print(f"Name: {character['name']}")
-    print(f"Bio: {character['description']}")
+        print(f"Name: {character['name']}")
+        print(f"Bio: {character['description']}")
 
